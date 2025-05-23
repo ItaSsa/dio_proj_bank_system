0ce529d83c45098f356cb4764c26b1ecf207256a
itairassa@gmail.com <itairassa@gmail.com>
2025-05-23 19:57:11 +0200

    feat: procedural_input functions implemetation

---
+++ procedural_input_version/Runing_instructions.md
@@ -0,0 +1,8 @@
+## How to run it?
+
+Go to directory and run the bank_system.py and:
+- If you are using poetry:
+
+    `poetry run python bank_system.py`
+- Or else:
+    ` python bank_system.py`
--- procedural_input_version/bank_system.py
+++ procedural_input_version/bank_system.py
@@ -10,22 +10,78 @@ menu = """
 """
 balance = 0.0
 daily_limit = 500
-statment = ""
+
+statement = ""
 withdraw_counting = 0
 COUNT_WITHDRAW_DAY = 3
 
+
+"""Fuction that implements deposit operation under following rules:
+    - Only possible to withdraw positive amounts.
+
+    - All deposits should be stored in one variable and view in the 
+    view statement operation.
+"""
+def do_deposit(_dep_amout):
+    if dep_amount <= 0:
+        print("The amout to deposit should be positive!")
+        print("Operation not conclude!")
+        exit
+    balance += dep_amount
+    statement += f""" \nDeposit of: $ {dep_amount}. 
+                      New balance: {balance} """ 
+    
+    print(f"Deposit of /$ { dep_amount } succesfully done! ")
+
+
+"""Fuction that implements withdraw operation under following rules:
+    -  Only 3 withdraws per day.
+
+    - Withdraw daily limit $500.
+
+    - If there is no balance the system abort the withdraw operation and send a message.
+"""
+def do_withdraw(_with_amount):
+    if withdraw_counting >= 3:
+        print("Daily count for withdraw reached")
+        exit
+  
+    if _with_amount >= daily_limit:
+        print(f"Only withdraw until ${daily_limit} are permited!")
+        exit
+    
+    if balance < _with_amount:
+        print(f" Amount for withdraw ${daily_limit} is major then the account balance ${balance}!")
+        exit   
+    
+    balance += _with_amount
+    withdraw_counting += 1
+    statement += f""" \nWithdraw of: $ {_with_amount}. 
+                New balance: {balance} """    
+
+def check_statememt():
+    print("Here the statment of your account: ")
+    print(statement)
+
 while True:
 
-    _option = input(menu)
+    option = input(menu)
 
-    if _option == "d":
-        print()
+    if option == "d":
+        message_deposit = "Please inform the amount for deposit: "
+        dep_amount = input(message_deposit)
+        do_deposit(dep_amount)
     
-    elif _option == "w":
-        print()
-    elif _option == "s":
-        print()
-    elif _option == "q":
-        print()
+    elif option == "w":
+        message_withdraw = "Please inform the amount for withdraw: "
+        with_amount = input(message_withdraw)
+        do_deposit(with_amount)
+        
+
+    elif option == "s":
+        check_statememt()
+        
+    elif option == "q":
+        print("Bye!")
     else:
         print(" Please choose one option from the menu ;]")
